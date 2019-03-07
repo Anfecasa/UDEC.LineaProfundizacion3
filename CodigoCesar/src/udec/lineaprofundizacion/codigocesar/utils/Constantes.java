aab4c1b0215c7dac15a20a27f934db662ca51f86
57320 <57320@LAPTOP-V9OGQVPK>
2019-03-07 00:53:46 -0500

    ndres eliminacion del proyecto por que contenia los .class

--- CodigoCesar/src/udec/lineaprofundizacion/codigocesar/utils/Constantes.java
+++
@@ -1,8 +0,0 @@
-package udec.lineaprofundizacion.codigocesar.utils;
-
-public class Constantes {
-	
-	public static final int POSICIONES=2;
-	public static final String[] ABECEDARIO = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-
-}
